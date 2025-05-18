c546a7bea9c962fa47b3aeb05b6f489937021206
Ayb001 <aghjedimayoub@gmail.com>
2025-05-18 21:19:55 +0100

    g

--- fe_baprjct/src/App.js
+++ fe_baprjct/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Home from './pages/home'
-import Login from './pages/login';
 import PrivacyPolicy from './pages/privacy_policy'
 import TermsConditions from './pages/terms_conditions'
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
@@ -13,7 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms-conditions" element={<TermsConditions />} />
-        <Route path="/login" element={<Login />} />
+
       </Routes>
       </BrowserRouter>
     </div>
