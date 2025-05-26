367eb896273e47b1b17137e254732b395e3ad68f
Ayb001 <aghjedimayoub@gmail.com>
2025-05-26 10:33:30 +0100

    new things

--- fe_baprjct/src/pages/login.jsx
+++ fe_baprjct/src/pages/login.jsx
@@ -1,92 +1,76 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
 
-function Login() {
+const Login = () => {
   return (
-    <>
-      <Helmet>
-        <link rel="stylesheet" href="/login.css" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
-      </Helmet>
-
+    <div className="login-page">
       <div className="login-container">
-        <input type="checkbox" id="flip" />
+        <input type="checkbox" id="login-flip" />
         <div className="login-cover">
           <div className="login-front">
-            <img src="images/frontImg.jpg" alt="" />
+            <img src="/images/frontImg.jpg" alt="" />
             <div className="login-text">
-              <span className="login-text-1">Every new friend is a <br /> new adventure</span>
-              <span className="login-text-2">Let's get connected</span>
+              <span className="login-text-1">Révélons le potentiel de <br/> Drâa-Tafilalet.</span>
+              <span className="login-text-2">Connectez-vous aux opportunités qui transforment notre région.</span>
             </div>
           </div>
           <div className="login-back">
-            <img className="login-backImg" src="images/backImg.jpg" alt="" />
+            <img className="backImg" src="/images/backImg.jpg" alt="" />
             <div className="login-text">
-              <span className="login-text-1">Complete miles of journey <br /> with one step</span>
-              <span className="login-text-2">Let's get started</span>
+              <span className="login-text-1">Réalisez de grandes choses<br /> en commençant aujourd'hui</span>
+              <span className="login-text-2">Lancez-vous dès maintenant</span>
             </div>
           </div>
         </div>
-
         <div className="login-forms">
           <div className="login-form-content">
             <div className="login-form">
-              <div className="login-title">Login</div>
-              <form>
+              <div className="login-title">Connexion</div>
+              <form action="#">
                 <div className="login-input-boxes">
                   <div className="login-input-box">
                     <i className="fas fa-envelope"></i>
-                    <input type="text" placeholder="Enter your email" required />
+                    <input type="text" placeholder="Entrez votre e-mail" required />
                   </div>
                   <div className="login-input-box">
                     <i className="fas fa-lock"></i>
-                    <input type="password" placeholder="Enter your password" required />
+                    <input type="password" placeholder="Entrez votre mot de passe" required />
                   </div>
-                  <div className="login-text"><a href="#">Forgot password?</a></div>
+                  <div className="login-text"><a href="#">Mot de passe oublié ?</a></div>
                   <div className="login-button login-input-box">
                     <input type="submit" value="Submit" />
                   </div>
-                  <div className="login-text login-sign-up-text">
-                    Don't have an account? <label htmlFor="flip">Signup now</label>
-                  </div>
+                  <div className="login-text login-sign-up-text">Vous n'avez pas de compte ? <label htmlFor="login-flip">Inscrivez-vous maintenant</label></div>
                 </div>
               </form>
             </div>
-
             <div className="login-signup-form">
-              <div className="login-title">Signup</div>
-              <form>
+              <div className="login-title">Créer un compte</div>
+              <form action="#">
                 <div className="login-input-boxes">
                   <div className="login-input-box">
                     <i className="fas fa-user"></i>
-                    <input type="text" placeholder="Enter your name" required />
+                    <input type="text" placeholder="Entrez votre nom" required />
                   </div>
                   <div className="login-input-box">
                     <i className="fas fa-envelope"></i>
-                    <input type="text" placeholder="Enter your email" required />
+                    <input type="text" placeholder="Entrez votre adresse e-mail" required />
                   </div>
                   <div className="login-input-box">
                     <i className="fas fa-lock"></i>
-                    <input type="password" placeholder="Enter your password" required />
+                    <input type="password" placeholder="Entrez votre mot de passe" required />
                   </div>
                   <div className="login-button login-input-box">
                     <input type="submit" value="Submit" />
                   </div>
-                  <div className="login-text login-sign-up-text">
-                    Already have an account? <label htmlFor="flip">Login now</label>
-                  </div>
+                  <div className="login-text login-sign-up-text">Vous avez déjà un compte ?<label htmlFor="login-flip">Connectez-vous</label></div>
                 </div>
               </form>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
-}
+};
 
 export default Login;
