78cf11bce755dd88a41233ac4b4f236818d7fcda
Ayb001 <aghjedimayoub@gmail.com>
2025-05-27 01:10:33 +0100

    test

--- fe_baprjct/src/App.js
+++ fe_baprjct/src/App.js
@@ -3,6 +3,8 @@ import Home from './pages/home'
 import PrivacyPolicy from './pages/privacy_policy'
 import TermsConditions from './pages/terms_conditions'
 import Login from './pages/login'
+import ForgotPassword from './pages/forgot_password'
+import ProjectCatalog from './pages/project_catalog'
 import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms-conditions" element={<TermsConditions />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/forgot_password" element={<ForgotPassword />}/>
+        <Route path="/project_catalog" element={<ProjectCatalog />} />
       </Routes>
       </BrowserRouter>
     </div>
